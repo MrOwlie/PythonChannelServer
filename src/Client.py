092cb6795d552836db9b057231c86dd692a897d7
= <=>
2015-11-10 19:12:53 +0100

    Added print to Client.py

--- src/Client.py
+++ src/Client.py
@@ -2,11 +2,12 @@ import threading
 from PacketHandler import handlePacket
 class Client:
     def __init__(self, connection, address):
-        self.connection = connection;
-        self.address = address;
+        self.connection = connection
+        self.address = address
 
     def read(self):
         while True:
             data = self.connection.recv(10000)
+            print("data: " + data)
             if data and (data != ""):
                 threading.Thread(target=handlePacket, args=(data,)).start()
